e9243f7041c0d5ea2facd4ed5661448e804084a1
MallamVamshiKrishna <88277957+MallamVamshiKrishna@users.noreply.github.com>
2026-04-22 23:44:43 +0530

    Update ingest.py

--- ingestion/adls_batch_ingestion/scripts/ingest.py
+++ ingestion/adls_batch_ingestion/scripts/ingest.py
@@ -8,8 +8,8 @@ df['tpep_dropoff_datetime']=pd.to_datetime(df['tpep_dropoff_datetime'])
 df['pickup_date']=df['tpep_pickup_datetime'].dt.floor('D')
 df['dropoff_date']=df['tpep_dropoff_datetime'].dt.floor('D')
 
-print(df.head())
-print(df.dtypes)
+print(df.headhead())
+print(df.dtypesdtypes)
 
 df.to_parquet('data/yellow_tripdata.parquet', engine='pyarrow',index='False')
 
