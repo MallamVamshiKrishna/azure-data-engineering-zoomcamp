88d9f5ecef186354dc1208e49adf38bbc0c69fea
MallamVamshiKrishna <88277957+MallamVamshiKrishna@users.noreply.github.com>
2026-04-29 23:14:13 +0530

    Update upload_to_adls.py

--- ingestion/adls_batch_ingestion/scripts/upload_to_adls.py
+++ ingestion/adls_batch_ingestion/scripts/upload_to_adls.py
@@ -8,25 +8,3 @@ def process_all_files():
             upload_to_adls(parquet_path, parquet_name)
 
     print("\nAll files processed successfully!")
-
-
-# from azure.storage.blob import BlobServiceClient
-# account_name = "vamshiadls123"
-# account_key = "YOUR_ACCESS_KEY"
-# container_name = "nyc-taxi-data"
-
-
-
-
-
-# connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
-
-# blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-# container_client = blob_service_client.get_container_client(container_name)
-
-# file_path = "data/yellow_tripdata.parquet"
-# blob_name = "yellow_tripdata.parquet"
-
-# with open(file_path, "rb") as data:
-#     container_client.upload_blob(name=blob_name, data=data, overwrite=True)
