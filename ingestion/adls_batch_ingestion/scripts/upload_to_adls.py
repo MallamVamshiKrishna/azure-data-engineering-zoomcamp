e8c5615ba89931f5175ddb59992f4801398173d9
MallamVamshiKrishna <88277957+MallamVamshiKrishna@users.noreply.github.com>
2026-04-29 23:13:26 +0530

    Update upload_to_adls.py

--- ingestion/adls_batch_ingestion/scripts/upload_to_adls.py
+++ ingestion/adls_batch_ingestion/scripts/upload_to_adls.py
@@ -1,77 +1,13 @@
-import os
-from azure.storage.blob import BlobServiceClient
+def process_all_files():
+    for file in os.listdir(DATA_DIR):
+        if file.endswith(".csv") or file.endswith(".csv.gz"):
 
+            file_path = os.path.join(DATA_DIR, file)
 
+            parquet_path, parquet_name = convert_to_parquet(file_path)
+            upload_to_adls(parquet_path, parquet_name)
 
-# -------------------------------
-# CONFIGURATION
-# -------------------------------
-ACCOUNT_NAME = "vamshiadls123"
-ACCOUNT_KEY ="YOUR_ACCESS_KEY"
-CONTAINER_NAME = "nyc-taxi-data"
-FILE_NAME = "yellow_tripdata.parquet"
-
-
-# -------------------------------
-# FUNCTION: Get File Path
-# -------------------------------
-def get_file_path(file_name):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    base_dir = os.path.dirname(current_dir)
-
-    file_path = os.path.join(base_dir, "data", file_name)
-
-    print(f"[INFO] File path resolved: {file_path}")
-    return file_path
-
-
-# -------------------------------
-# FUNCTION: Connect to Azure
-# -------------------------------
-def get_container_client():
-    connection_string = (
-        f"DefaultEndpointsProtocol=https;"
-        f"AccountName={ACCOUNT_NAME};"
-        f"AccountKey={ACCOUNT_KEY};"
-        f"EndpointSuffix=core.windows.net"
-    )
-
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(CONTAINER_NAME)
-
-    print("[INFO] Connected to Azure Storage")
-    return container_client
-
-
-# -------------------------------
-# FUNCTION: Upload File
-# -------------------------------
-def upload_file(container_client, file_path, blob_name):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"[ERROR] File not found: {file_path}")
-
-    print(f"[INFO] Uploading file: {blob_name}")
-
-    with open(file_path, "rb") as data:
-        container_client.upload_blob(name=blob_name, data=data, overwrite=True)
-
-    print("[SUCCESS] File uploaded successfully!")
-
-
-# -------------------------------
-# MAIN FUNCTION
-# -------------------------------
-def main():
-    file_path = get_file_path(FILE_NAME)
-    container_client = get_container_client()
-    upload_file(container_client, file_path, FILE_NAME)
-
-
-# -------------------------------
-# ENTRY POINT
-# -------------------------------
-if __name__ == "__main__":
-    main()
+    print("\nAll files processed successfully!")
 
 
 # from azure.storage.blob import BlobServiceClient
